2521b33577e974ef65578aa79ec204ec166190a1
guilhermescr <guilhermechaves2005@hotmail.com>
2022-08-22 15:12:04 -0300

    added my first exercise

---
+++ exercises/ex001.js
@@ -0,0 +1,6 @@
+console.log('Meu nome é "Guilherme". Estou aprendendo JavaScript às', 14, 'da tarde.');
+
+const myName = "Guilherme";
+let hours = new Date().getHours();
+
+console.log(`My name is ${myName}. I'm learning JavaScript at ${hours}h.`);
