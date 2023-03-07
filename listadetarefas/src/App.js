6a3ff7e5f14de53405fe14815c55513f8fcdcf27
guilhermescr <guilhermechaves2005@hotmail.com>
2023-03-07 11:56:06 -0300

    added tailwind cli to listadetarefas/

--- listadetarefas/src/App.js
+++ listadetarefas/src/App.js
@@ -1,9 +1,18 @@
-function App() {
+import React from 'react';
+import './main.css';
+
+export default function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="bg-gray-200 min-h-screen flex flex-col justify-between">
+      <h1 className="bg-gray-900 text-5xl text-gray-300 text-center p-4">
+        Hello, world!
+      </h1>
+
+      <img
+        className="max-w-xs w-full mx-auto my-auto"
+        src="https://cdn.britannica.com/91/181391-050-1DA18304/cat-toes-paw-number-paws-tiger-tabby.jpg"
+        alt="Serious Cat"
+      />
     </div>
   );
 }
-
-export default App;
